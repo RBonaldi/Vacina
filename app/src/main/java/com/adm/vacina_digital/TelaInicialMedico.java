24c18b7b442701c7c7afb8eed1dbd6c961163016
Rafael Bonaldi <rafaelbonaldi@gmail.com>
2017-06-26 00:51:02 -0300

    Tela pesquisa

--- app/src/main/java/com/adm/vacina_digital/TelaInicialMedico.java
+++ app/src/main/java/com/adm/vacina_digital/TelaInicialMedico.java
@@ -2,6 +2,7 @@ package com.adm.vacina_digital;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,6 +18,7 @@ public class TelaInicialMedico extends AppCompatActivity implements View.OnClick
     ListView lista;
     List<String> vacina;
     private CPF cpf;
+    EditText txtCPF;
 
     private Banco banco;
 
@@ -33,18 +35,28 @@ public class TelaInicialMedico extends AppCompatActivity implements View.OnClick
         Button btMarcacao = (Button)findViewById(R.id.bt_marcacao);
         btMarcacao.setOnClickListener(this);
 
+        Button btPesquisar = (Button)findViewById(R.id.bt_Pesquisar);
+        btPesquisar.setOnClickListener(this);
+
+        txtCPF = (EditText)findViewById(R.id.txt_pesquisarCPF);
+
+        cpf = new CPF(this);
+
         lista = (ListView) findViewById(R.id.lista);
 
         vacina = new ArrayList<>();
-        vacina.add("Brasileiro");
-        vacina.add("Libertadores");
-        vacina.add("Mineiro");
-        vacina.add("Espanhol");
-        vacina.add("Paulista");
+
+        Cursor cursor = banco.lista_cartao_vacina(cpf.getusename());
+
+        while (cursor.moveToNext()){
+            vacina.add(cursor.getString(1) + " - " + cursor.getString(2));
+        }
 
         ListaAdapter adapter = new ListaAdapter(vacina, this);
 
         lista.setAdapter(adapter);
+
+        txtCPF.setText(cpf.getusename());
     }
 
     @Override
@@ -59,6 +71,10 @@ public class TelaInicialMedico extends AppCompatActivity implements View.OnClick
             case R.id.bt_marcacao:
                 marcacao();
                 break;
+
+            case R.id.bt_Pesquisar:
+                pesquisa();
+                break;
         }
     }
 
@@ -72,7 +88,14 @@ public class TelaInicialMedico extends AppCompatActivity implements View.OnClick
         startActivity(intent);
     }
 
+    private  void pesquisa(){
+        cpf = new CPF(this);
+        cpf.setusename(txtCPF.getText().toString());
 
+        Intent intent = new Intent(TelaInicialMedico.this, TelaInicialMedico.class);
+        startActivity(intent);
+        finish();
+    }
 
 
 
